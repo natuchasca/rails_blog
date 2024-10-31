dbfce95c83b45b477edb6096548d61914ae2e415
natuchasca <natuxalee@gmail.com>
2024-10-31 00:13:01 -0300

    [show and new articles actions]

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,4 +2,27 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(articles_params)
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def articles_params
+    params.require(:article).permit(:title, :body)
+  end
 end
